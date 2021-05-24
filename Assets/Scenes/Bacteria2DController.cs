ede58c88c6663360c31dcaba706657f7fe9d9f9d
Henrique Shiguemoto <azrick88@outlook.com>
2021-05-23 21:07:37 -0300

    Setting up the test environment, player controls and camera following

---
+++ Assets/Scenes/Bacteria2DController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bacteria2DController : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody2D;
+    private Vector3 moveDir;
+    private const float MOVE_SPEED = 10f;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1f;
+        }
+
+        moveDir = new Vector3(moveX, moveY).normalized;
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = moveDir*MOVE_SPEED;
+    }
+}
